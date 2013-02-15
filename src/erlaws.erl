702766fa9ed0382b0990ce229e71d4f2dedfde92
Johan Persson <johan@programlabbet.se>
2013-02-15 13:42:50 +0100

    Bugfixes and rebarified

--- src/erlaws.erl
+++ src/erlaws.erl
@@ -1,16 +1,21 @@
--module(erlaws). 
+-module(erlaws).
+-export([start/0, stop/0]).
 
--behaviour(application). 
-
--export([start/0, start/2, stop/1]). 
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
 
 start() ->
-    application:start(sasl),
-    crypto:start(),
-    inets:start().
+    erlaws_deps:ensure(),
+    ensure_started(sasl),
+    ensure_started(crypto),
+    ensure_started(inets),
+    application:start(erlaws).
 
-start(_Type, _Args) -> 
-	erlaws:start().
-	
-stop(_State) -> 
-    ok. 
+stop() ->
+    application:stop(erlaws).
+
---
+++ src/erlaws_app.erl
@@ -0,0 +1,11 @@
+-module(erlaws_app).
+
+-behaviour(application). 
+
+-export([start/2, stop/1]). 
+
+start(_Type, _Args) ->
+    erlaws_deps:ensure().
+	
+stop(_State) -> 
+    ok. 
